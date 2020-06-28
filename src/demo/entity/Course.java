4d1d9e5d2a817e3078282322ce370740b9386a0d
debiprasadmishra50 <debiprasadmishra50@gmail.com>
2020-06-29 00:49:10 +0530

    Hibernate Eager Lazy

---
+++ src/demo/entity/Course.java
@@ -0,0 +1,66 @@
+package demo.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "course")
+public class Course {
+	// define the fields, constructors, getters and setters, toString and add the annotation
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "title")
+	private String title;
+	
+	// Many Courses can have One instructor
+	@ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+	@JoinColumn(name = "instructor_id") // create a column instructor_id, add foreign key to it with id column of instructor Table
+	private Instructor instructor;
+	
+	public Course() {
+	}
+
+	public Course(String title) {
+		this.title = title;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Instructor getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
+	}
+
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", title=" + title + ", instructor=" + instructor + "]";
+	}
+}
